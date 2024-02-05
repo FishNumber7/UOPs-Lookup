fcb84532b00501da3155710a3f4ab5993ba4a81f
Sam Colvin <hmc3cx@virginia.edu>
2024-02-05 18:09:02 -0500

    basic code in uopParser - need to put in header

--- uopParser.cpp
+++ uopParser.cpp
@@ -1,17 +1,44 @@
 #include "pugixml.hpp"
 #include <iostream>
 
-int[22] ignore_list = {'AMD_INVLPGB', 'AMX_BF16', 'AMX_INT8', 'AMX_TILE', 'AVX_IFMA', 'AVX_NE_CONVERT', 'AVX_VNNI', 'AVX_VNNI_INT8', 'CMPCCXADD', 'ENQCMD', 'HRESET', 'ICACHE_PREFETCH', 'KEYLOCKER', 'KEYLOCKER_WIDE', 'MSRLIST', 'RAO_INT', 'SERIALIZE', 'SNP', 'TDX', 'TSX_LDTRK', 'UINTR', 'WRMSRNS'};
+class UOP_Lookup{
 
-int main()
-{
-    pugi::xml_document doc;
-    pugi::xml_parse_result result = doc.load_file("instructions.xml");
-    if (!result)
-        return -1;
+public:
 
-    for (pugi::xml_node tool: doc.children("instruction")) {
-        if ({})
+    void set_architecture(std::string architecture) 
+    {
+        arch = architecture; 
     }
 
-}
+    int lookup(std::string instruction)
+    {
+        if (result) {
+            this.instruction = instruction;
+            return doc.find_child(find_instruction).find_child(find_arch).first_child().find_attribute(find_uops).value();
+        }
+    }
+
+
+private:
+
+    pugi::xml_document doc;
+    pugi::xml_parse_result result = doc.load_file("instructions.xml");
+    std::string arch;
+    std:string instruction  = "";
+
+    bool find_instruction(pugi::xml_node node) 
+    {
+        return strcmp(node.attribute("string"), instruction) == 0;
+    }
+
+    bool find_arch(pugi::xml_node node)
+    {
+        return strcmp(node.attribute("name"), arch) == 0;
+    }
+
+    bool find_uops(pugi::xml_attribute attr)
+    {
+        return strcmp(attr.name(), "uops") == 0;
+    }
+
+};
