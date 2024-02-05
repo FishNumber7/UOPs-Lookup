634deefc9d6156db8b32643b4eb80ae5dd890464
Sam Colvin <samuelcolvin26@gmail.com>
2024-02-05 16:06:18 -0500

    beginning code

--- uopParser.cpp
+++ uopParser.cpp
@@ -1,7 +1,17 @@
 #include "pugixml.hpp"
 #include <iostream>
 
+int[22] ignore_list = {'AMD_INVLPGB', 'AMX_BF16', 'AMX_INT8', 'AMX_TILE', 'AVX_IFMA', 'AVX_NE_CONVERT', 'AVX_VNNI', 'AVX_VNNI_INT8', 'CMPCCXADD', 'ENQCMD', 'HRESET', 'ICACHE_PREFETCH', 'KEYLOCKER', 'KEYLOCKER_WIDE', 'MSRLIST', 'RAO_INT', 'SERIALIZE', 'SNP', 'TDX', 'TSX_LDTRK', 'UINTR', 'WRMSRNS'};
+
 int main()
 {
+    pugi::xml_document doc;
+    pugi::xml_parse_result result = doc.load_file("instructions.xml");
+    if (!result)
+        return -1;
+
+    for (pugi::xml_node tool: doc.children("instruction")) {
+        if ({})
+    }
 
 }
