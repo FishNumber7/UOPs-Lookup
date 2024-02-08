89a0a33a69ef60a5599a31ef423b9fda2d002922
Sam Colvin <hmc3cx@virginia.edu>
2024-02-07 21:47:53 -0500

    removed extraneous include

--- uopParser.hpp
+++ uopParser.hpp
@@ -1,8 +1,6 @@
 #ifndef UOPPARSER_HPP
 #define UOPPARSER_HPP
 
-#include <stdint.h>
-
 const char* lookup(const char* instruction, const char* op1_size, const char* op2_size, const char* op3_size, const char* architecture);
 
 #endif
